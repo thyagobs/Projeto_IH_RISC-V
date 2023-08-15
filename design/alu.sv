75fd4bf7bf18cf821b5f82bd18e10060e354da2e
Thyago Soares <114110392+thyagobs@users.noreply.github.com>
2023-08-15 07:16:17 -0300

    Up alu.sv

--- design/alu.sv
+++ design/alu.sv
@@ -18,7 +18,7 @@ module alu#(
             4'b0000:        // AND
                     ALUResult = SrcA & SrcB;
 	    4'b0001:        // OR
-		    ALUResult = SrcA || SrcB;	
+		    ALUResult = SrcA | SrcB;	
             4'b0010:        // ADD
                     ALUResult = SrcA + SrcB;
 	    4'b0011:	    // XOR
