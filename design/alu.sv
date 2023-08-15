c52da11a97028141a3a5e4f669be2f64cd13aec3
gvnna <giovannacssv@hotmail.com>
2023-08-14 21:18:09 -0300

    add instrucoes logicas/aritm sem testar com instrucoes

--- design/alu.sv
+++ design/alu.sv
@@ -17,10 +17,23 @@ module alu#(
             case(Operation)
             4'b0000:        // AND
                     ALUResult = SrcA & SrcB;
+	    4'b0001:        // OR
+		    ALUResult = SrcA || SrcB;	
             4'b0010:        // ADD
                     ALUResult = SrcA + SrcB;
-            4'b1000:        // Equal
+	    4'b0011:	    // XOR
+	            ALUResult = SrcA ^ SrcB;
+            4'b0100:	    // SUB
+		    ALUResult = SrcA - SrcB;
+            4'b0101:        // BNE
+                    ALUResult = (SrcA != SrcB) ? 1 : 0;
+	    4'b0110:        // BLT 
+                    ALUResult = (SrcA < SrcB) ? 1 : 0;
+	    4'b0111:        // BGE 
+                    ALUResult = (SrcA > SrcB) ? 1 : 0;
+            4'b1000:        // BEQ
                     ALUResult = (SrcA == SrcB) ? 1 : 0;
+	
             default:
                     ALUResult = 0;
             endcase
