d56bde9c22a5bbe9c64c7dcaebe3cb45393be532
Thyago Soares <114110392+thyagobs@users.noreply.github.com>
2023-08-23 08:16:21 -0300

    Implementando jals

--- design/Controller.sv
+++ design/Controller.sv
@@ -15,12 +15,15 @@ module Controller (
     output logic RegWrite, //The register on the Write register input is written with the value on the Write data input 
     output logic MemRead,  //Data memory contents designated by the address input are put on the Read data output
     output logic MemWrite, //Data memory contents designated by the address input are replaced by the value on the Write data input.
-    output logic [1:0] ALUOp,  //00: LW/SW; 01:Branch; 10: Rtype; 11: Utype
-    output logic Branch  //0: branch is not taken; 1: branch is taken
+    output logic [1:0] ALUOp,  //00: LW/SW ou jalr; 01:Branch; 10: Rtype; 11: Utype e jal
+    output logic Branch,  //0: branch is not taken; 1: branch is taken
+    output logic jals //0: jal is not taken; 1: jal is taken
 );
 
-  logic [6:0] R_TYPE, LW, SW, BR, I_TYPE, U_TYPE;
+  logic [6:0] R_TYPE, LW, SW, BR, I_TYPE, U_TYPE, JAL, JALR;
 
+  assign JAL = 7'b1101111;  //jal
+  assign JALR = 7'b1100111;  //jalr
   assign U_TYPE = 7'b0110111;  //lui
   assign I_TYPE = 7'b0010011;  //slli, addi
   assign R_TYPE = 7'b0110011;  //add,and
@@ -28,13 +31,14 @@ module Controller (
   assign SW = 7'b0100011;  //sw 
   assign BR = 7'b1100011;  //beq
 
-  assign ALUSrc = (Opcode == LW) || (Opcode == SW) || (Opcode == I_TYPE) || (Opcode == U_TYPE);
+  assign ALUSrc = (Opcode == LW) || (Opcode == SW) || (Opcode == I_TYPE) || (Opcode == U_TYPE) || (Opcode == JAL) || (Opcode == JALR);
   assign MemtoReg = (Opcode == LW);
   assign RegWrite = (Opcode == R_TYPE) || (Opcode == LW) || (Opcode == I_TYPE) || (Opcode == U_TYPE);
   assign MemRead = (Opcode == LW);
   assign MemWrite = (Opcode == SW);
-  assign ALUOp[0] = (Opcode == BR) || (Opcode == U_TYPE);
-  assign ALUOp[1] = (Opcode == R_TYPE) || (Opcode == I_TYPE) || (Opcode == U_TYPE);
+  assign ALUOp[0] = (Opcode == BR) || (Opcode == U_TYPE) || (Opcode == JAL);
+  assign ALUOp[1] = (Opcode == R_TYPE) || (Opcode == I_TYPE) || (Opcode == U_TYPE) || (Opcode == JAL);
   assign Branch = (Opcode == BR);
+  assign jals = (Opcode == JAL) || (Opcode == JALR);
 
 endmodule
