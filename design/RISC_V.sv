df5e0b66d9c517b9beb4b8ac9c58a4fd39033b63
Thyago Soares <114110392+thyagobs@users.noreply.github.com>
2023-08-23 08:16:54 -0300

    Implementando jals

--- design/RISC_V.sv
+++ design/RISC_V.sv
@@ -19,7 +19,7 @@ module riscv #(
 );
 
   logic [6:0] opcode;
-  logic ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch;
+  logic ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, jals;
   logic [1:0] ALUop;
   logic [1:0] ALUop_Reg;
   logic [6:0] Funct7;
@@ -34,14 +34,16 @@ module riscv #(
       MemRead,
       MemWrite,
       ALUop,
-      Branch
+      Branch,
+      jals
   );
 
   ALUController ac (
       ALUop_Reg,
       Funct7,
       Funct3,
-      Operation
+      Operation,
+      jals
   );
 
   Datapath dp (
@@ -67,7 +69,8 @@ module riscv #(
       rd,
       addr,
       wr_data,
-      rd_data
+      rd_data,
+      jals
   );
 
 endmodule
