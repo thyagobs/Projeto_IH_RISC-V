eb5369de25cecd7967ca6f854d5469951a6af893
Thyago Soares <114110392+thyagobs@users.noreply.github.com>
2023-08-23 08:14:11 -0300

    Implementando jals

--- design/ALUController.sv
+++ design/ALUController.sv
@@ -7,7 +7,8 @@ module ALUController (
     input logic [2:0] Funct3,  // bits 12 to 14 of the instruction
 
     //Output
-    output logic [3:0] Operation  // operation selection for ALU
+    output logic [3:0] Operation,  // operation selection for ALU
+    input logic jals
 );
 
   assign Operation[0] = ((ALUOp == 2'b10) && (Funct3 == 3'b110) && (Funct7 == 7'b0000000)) ||  // OR
@@ -20,7 +21,8 @@ module ALUController (
       ((ALUOp == 2'b00)); //LW/SW
 
   assign Operation[1] = (ALUOp == 2'b00) ||  // LW\SW
-      (ALUOp == 2'b11) || // LUI
+      ((ALUOp == 2'b11) && (~jals)) || // LUI
+      ((ALUOp == 2'b11) && (jals)) || // JAL
       ((ALUOp == 2'b10) && (Funct3 == 3'b000) && (Funct7 != 7'b0100000)) ||  // ADD
       ((ALUOp == 2'b10) && (Funct3 == 3'b100) && (Funct7 == 7'b0000000)) || // XOR
       ((ALUOp == 2'b01) && (Funct3 == 3'b100)) || // BLT
@@ -29,6 +31,7 @@ module ALUController (
 
   assign Operation[2] = ((ALUOp == 2'b10) && (Funct3 == 3'b001)) ||  // SLLI
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 [5] == 0)) || // SRLI
+      ((ALUOp == 2'b11) && (jals)) || // JAL
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 [5] == 1)) || // SRAI
       ((ALUOp == 2'b01) && (Funct3 == 3'b100)) || // BLT
       ((ALUOp == 2'b01) && (Funct3 == 3'b101)) || //BGE
@@ -37,6 +40,7 @@ module ALUController (
  
   assign Operation[3] = ((ALUOp == 2'b01) && (Funct3 == 3'b000)) ||  // BEQ
       ((ALUOp == 2'b00)) || //LW/SW
+      ((ALUOp == 2'b11) && (jals)) || // JAL
       ((ALUOp == 2'b10) && (Funct3 == 3'b000) && (Funct7 != 7'b0100000)) ||  // ADD
       ((ALUOp == 2'b10) && (Funct3 == 3'b000) && (Funct7 == 7'b0100000)) || //SUB
       ((ALUOp == 2'b10) && (Funct3 == 3'b010)) || //SLT
