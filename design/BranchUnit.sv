e3272753ea858344bdee9fc9667f338246fe0352
Thyago Soares <114110392+thyagobs@users.noreply.github.com>
2023-09-02 18:21:37 -0300

    corrigindo errinho de interpretação

--- design/BranchUnit.sv
+++ design/BranchUnit.sv
@@ -21,7 +21,7 @@ module BranchUnit #(
 
   assign PC_Full = {23'b0, Cur_PC};
 
-  assign PC_Imm = jalRs ? PC_Full + AluResult : PC_Full + Imm;
+  assign PC_Imm = jalRs ? AluResult : PC_Full + Imm;
   assign PC_Four = PC_Full + 32'b100;
   assign Branch_Sel = jalRs ? Branch : Branch && AluResult[0];  // 0:Branch or jal is taken; 1:Branch or jal is not taken
   assign JalSel = jals || jalRs;
